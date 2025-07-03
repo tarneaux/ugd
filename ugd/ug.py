c396f2949b30c5f8b61c0d5851d5d19254df7b48
tarneo <tarneo@tarneo.fr>
2025-07-03 16:44:39 +0200

    Add license header to ug.py

--- ugd/ug.py
+++ ugd/ug.py
@@ -1,3 +1,18 @@
+# freetar - an alternative frontend to ultimate-guitar.com
+# Copyright (C) 2025 kmille
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import quote, urlparse
